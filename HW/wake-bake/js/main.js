60df380f9653d726f335761aa3d29414026accac
lstmind <aaa.kunt@mail.ru>
2025-10-15 22:26:19 +0300

    Add files via upload

--- HW/wake-bake/js/main.js
+++ HW/wake-bake/js/main.js
@@ -134,4 +134,32 @@
         }
 
     });
+
+    // Testimonials
+
+        new Swiper('.testimonials__slider', {
+
+        spaceBetween: 0,
+        slidesPerView: 1,
+        centeredSlides: true,
+
+        navigation: {
+            nextEl: '.testimonials__next',
+            prevEl: '.testimonials__prev',
+        },
+
+        scrollbar: {
+            el: '.swiper-scrollbar',
+            draggable: true,
+        },
+
+        breakpoints: {
+            901: {
+                slidesPerView: 1.5,
+            },
+            1201: {
+                slidesPerView: 2.1,
+            }
+        }
+    });
 })()
